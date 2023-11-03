63c234a11f08dd2c51cf926915e7bdba2345a7b8
Emil Sultanov <100-44@mail.ru>
2023-11-03 21:58:58 +0500

    Deleted file

--- SEM_project_01.py
+++
@@ -1,5 +0,0 @@
-#Эта модель оптимизирована для классификации эмоций коротких русских текстов.
-
-from transformers import pipeline
-model = pipeline(model="seara/rubert-tiny2-ru-go-emotions")
-model("Привет, ты мне нравишься!")
