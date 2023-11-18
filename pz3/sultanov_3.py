81c1d64c60959a663c1e0a4dad497ff8c50a25e7
Emil Sultanov <100-44@mail.ru>
2023-11-18 14:09:58 +0500

    added pz3 folder and sultanov_3.py file

---
+++ pz3/sultanov_3.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-mul-en")
+
+class Item(BaseModel):
+    text: str
+    
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/translate/")
+def translate(item: Item):
+    return pipe(item.text)[0]
+
+
+
