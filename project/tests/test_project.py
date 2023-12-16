8cc3dc324ea6b22e27ba336333eeef2c2ea69a75
skittree <pdimonn@mail.ru>
2023-12-16 16:07:26 +0500

    try move the env variable to step level

--- project/tests/test_project.py
+++ project/tests/test_project.py
@@ -4,26 +4,22 @@ from streamlit.testing.v1 import AppTest
 
 
 def test_key_in_env():
-    at = AppTest.from_file('project.py')
-    at.secrets['API_KEY_YOUTUBE'] = os.getenv('API_KEY_YOUTUBE')
-    assert at.secrets['API_KEY_YOUTUBE'] is not None
+    assert os.getenv('API_KEY_YOUTUBE') is not None
 
 def test_bad_url_input():
-    at = AppTest.from_file('project.py')
-    at.secrets['API_KEY_YOUTUBE'] = os.getenv('API_KEY_YOUTUBE')
-    at.run(timeout=10)
+    at = AppTest.from_file('project.py', default_timeout=10)
+    at.run()
     at.sidebar.text_input[0].input('123').run()
-    at.sidebar.button[0].click().run(timeout=10)
+    at.sidebar.button[0].click().run()
     assert len(at.sidebar.text_input) == 1
     assert at.warning[0].value == 'URL'
     assert at.success[0].value == 'Готово! Обработано 20 комментариев.'
 
 def test_url_input():
-    at = AppTest.from_file('project.py')
-    at.secrets['API_KEY_YOUTUBE'] = os.getenv('API_KEY_YOUTUBE')
-    at.run(timeout=10)
+    at = AppTest.from_file('project.py', default_timeout=10)
+    at.run()
     at.sidebar.text_input[0].input('https://www.youtube.com/watch?v=wDmPgXhlDIg').run()
-    at.sidebar.button[0].click().run(timeout=10)
+    at.sidebar.button[0].click().run()
     assert len(at.sidebar.text_input) == 1
     assert at.success[0].value == 'URL'
     assert at.success[1].value == 'Готово! Обработано 100 комментариев.'
