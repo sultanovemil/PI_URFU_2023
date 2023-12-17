ec089fbc985bcc769f48d102413cd452085165a4
skittree <pdimonn@mail.ru>
2023-12-17 12:59:43 +0500

    try creating secrets through actions

--- project/tests/test_project.py
+++ project/tests/test_project.py
@@ -5,11 +5,8 @@ from streamlit.testing.v1 import AppTest
 DEVELOPER_KEY = os.getenv('API_KEY_YOUTUBE')
 
 
-# def test_key_in_env():
-#     assert DEVELOPER_KEY is not None
-
 def test_bad_url_input():
-    at = AppTest.from_file('project.py', default_timeout=10)
+    at = AppTest.from_file('project.py', default_timeout=30)
     at.secrets['API_KEY_YOUTUBE'] = DEVELOPER_KEY
     at.run()
     at.sidebar.text_input[0].input('123').run()
@@ -19,7 +16,7 @@ def test_bad_url_input():
     assert at.success[0].value == 'Готово! Обработано 20 комментариев.'
 
 def test_url_input():
-    at = AppTest.from_file('project.py', default_timeout=10)
+    at = AppTest.from_file('project.py', default_timeout=30)
     at.secrets['API_KEY_YOUTUBE'] = DEVELOPER_KEY
     at.run()
     at.sidebar.text_input[0].input('https://www.youtube.com/watch?v=wDmPgXhlDIg').run()
